107b2f929e23feeee8ca7f651fc4f4b35c7f1e71
dipak2016 <tops1111@macs-imac.local>
2016-06-01 15:57:12 +0530

    Initial Commit

---
+++ TableViewGetdataSQLite/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TableViewGetdataSQLite
+//
+//  Created by Mac on 6/1/16.
+//  Copyright © 2016 Mac. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TableViewGetdataSQLite/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TableViewGetdataSQLite
+//
+//  Created by Mac on 6/1/16.
+//  Copyright © 2016 Mac. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
